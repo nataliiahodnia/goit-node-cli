7e716aa8e921169efcc0945e558b4f3b99f98c4d
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-06-24 12:31:20 +0200

    added step 3 and 4

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-import { program } from "commander";
+const { program } = require('commander');
+const { listContacts, getContactById, removeContact, addContact } = require('./contacts');
+
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
 
 program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
-    case "list":
-      // ...
+    case 'list':
+      const contacts = await listContacts();
+      console.log(contacts);
       break;
 
-    case "get":
-      // ... id
+    case 'get':
+      const contact = await getContactById(id);
+      console.log(contact);
       break;
 
-    case "add":
-      // ... name email phone
+    case 'add':
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
       break;
 
-    case "remove":
-      // ... id
+    case 'remove':
+      const removedContact = await removeContact(id);
+      console.log(removedContact);
       break;
 
     default:
-      console.warn("\x1B[31m Unknown action type!");
+      console.warn('\x1B[31m Unknown action type!');
   }
 }
 
